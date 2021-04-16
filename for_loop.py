16bfcd4fa68494a316892665cdc8755224e13f4f
alextzo0207@gmail.com <alextsai@gbd730.local>
2021-04-16 18:10:29 +0800

    just fun test

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+import random
+r = random.randint(1, 100)
+abc = [r, r]
+for a in abc:
+	print(a)
+cars = ['Toyota', 'Honda']
+for car in cars:
+	print(car)
+names = ['Allen', 'Tom', 'Jack']
+for name in names:
+	print(name)
